678f0c0ac9f63caa7d7daa9382d02d8cbf7d8768
spidunno <unnospid@gmail.com>
2024-05-27 20:06:52 -0400

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
-export default defineConfig({
-  plugins: [solid()],
-})
+export default defineConfig(({ mode }) => {
+	const common: UserConfig = {
+		plugins: [solid()],
+	};
+	if (mode === "gh-pages") {
+		return { ...common, base: "/sorting-algorithm-visualizer" };
+	} else {
+		return common;
+	}
+});
